b3536c26330c72e0051b69699777ac0447816303
alexripperton <rippertonalex@gmail.com>
2024-06-04 17:40:36 -0700

    comments

--- src/useCommandBar.ts
+++ src/useCommandBar.ts
@@ -8,6 +8,10 @@ import { editCompanyDetails } from './store/actions';
 
 init('c6396d27');
 
+// useCommandBar is the main functional component
+// vlatio is a state management library. It automatically updates the UI when the state changes.
+// snapshot is a snapshot of the current state to get info like leads stages etc
+// use navigate is a hook to route within the application
 const useCommandBar = () => {
   const snapshot = useSnapshot(_);
   const loggedInUserId = '424242';
@@ -15,14 +19,17 @@ const useCommandBar = () => {
 
   const navigate = useNavigate();
 
+  // useEffect is used to run side effects in the component
   React.useEffect(() => {
     function router(url: string) {
       navigate(url);
     }
 
     window.CommandBar.addRouter(router);
-  }, [navigate]);
+  }, [navigate]);  // dependency array showing how router is dependent on navigate
 
+  // this effect runs when the snapshot.companies changes
+  //  It adds a context to CommandBar with the key 'leads' and the value of snapshot.companies
   React.useEffect(() => {
     window.CommandBar.addContext('leads', snapshot.companies);
   }, [snapshot.companies]);
@@ -35,6 +42,7 @@ const useCommandBar = () => {
     window.CommandBar.addContext('activeLead', snapshot.activeCompany);
   }, [snapshot.activeCompany]);
 
+  // the empty dependency array means that this effect runs only once when the component is rendered
   React.useEffect(() => {
     window.CommandBar.addCallback('updateLeadStatus', (args: any, context: any) => {
       const companyId = context.activeLead.id;
